10ad0f6fdc80205d143cb0ef0736b96d5e39a49e
Akif Eyler <ma.eyler@gmail.com>
2015-11-06 09:20:01 +0300

    V1
    larger size

--- sayac/Sayac.java
+++ sayac/Sayac.java
@@ -1,29 +1,45 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.util.Date;
 import java.text.SimpleDateFormat;
 
 class Sayac implements Runnable {
-    JLabel disp;
-    boolean running;
+
+    static final int 
+        RESOLUTION = Toolkit.getDefaultToolkit().getScreenResolution(); 
+    static final float 
+        RES_RATIO = RESOLUTION/96f;  //default resolution is 96
+    static final int GAP = scaled(8);
+    static final Color COLOR = Color.yellow;
+    static final Font NORM = new Font("Serif", 0, scaled(50));
+    static final Font BIG  = new Font("Serif", 0, scaled(96));
+
+    final JLabel days = new JLabel();
+    final JLabel left = new JLabel("gün kaldý");
+    Thread thd;
     long target;
     public Sayac() {
         SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
         try {
-            java.util.Date d = df.parse("5.11.2028");
+            Date d = df.parse("5.11.2028");
             target = d.getTime();
         } catch(java.text.ParseException e) {
-            disp.setText("ParseException");
+            days.setText("ParseException");
         }
         makeFrame(); 
     }
+    void setDate() {
+        long diff = target - System.currentTimeMillis();
+        long d = diff/(24*3600);  //double
+        days.setText(String.format("%s", d/1000)); //"%.3f"
+    }
     public void run() {
         System.out.println("start");
-        running = true;
-        while (running) {
-            long diff = target - System.currentTimeMillis();
-            double d = diff/(24*3600);
-            disp.setText(String.format("%.3f", d/1000));
+        while (Thread.currentThread() == thd) {
             try {
+                setDate(); 
                 Thread.sleep(10*1000);
             } catch (InterruptedException e) {
 	    }
@@ -31,30 +47,38 @@ class Sayac implements Runnable {
         System.out.println("Stop");
     }
     public void start() {
-        if (running) return;
-        running = true;
-        new Thread(this).start();
+        if (Thread.currentThread() == thd) return;
+        thd = new Thread(this);
+        thd.start();
     }
     public void stop() {
-        running = false;
+        if (thd != null) thd.interrupt();
+        thd = null;
     }
     void makeFrame() {
         JPanel p = new JPanel();
         p.setBackground(Color.blue);
-        //p.setForeground(Color.yellow);
-	//p.setLayout(new GridLayout(2,1));
-        disp = new JLabel("xxxxx");
-        disp.setFont(new Font("Serif", 0, 24));
-        disp.setForeground(Color.yellow);
-        p.add(disp);
+	p.setLayout(new BorderLayout(0, 0));
+        p.setBorder(new EmptyBorder(0, GAP, GAP, GAP));
+        days.setFont(BIG);
+        days.setForeground(COLOR);
+        p.add(days, "North");
+        left.setFont(NORM);
+        left.setForeground(COLOR);
+        p.add(left, "South");
 
         JFrame f = new JFrame("Sayaç");  //a window
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //f.setLocation(0, 300);  //left and middle
-        f.setContentPane(p);   //add p to f
-        f.pack();   //minimal size
+        f.setContentPane(p); 
+        setDate(); f.pack();  //minimal size
         f.setVisible(true);   //show
+        
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) { stop(); }
+        });
     }
+
+    public static int scaled(int k) { return Math.round(k*RES_RATIO); }
     public static void main(String[] args) {
         new Sayac().start();;
     }
---
+++ sayac/make.bat
@@ -0,0 +1,4 @@
+\java\jdk7\bin\javac Sayac.java
+del Sayac.jar
+\java\jdk7\bin\jar cfm Sayac.jar MANIFEST.MF
+\java\jdk7\bin\jar uf  Sayac.jar *.class *.java
